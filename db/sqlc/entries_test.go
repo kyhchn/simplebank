3cd350de5dcf36e5d85b3f0a1dc53e01f5b74c74
Alvin KN <alvinkn.dev@gmail.com>
2023-01-08 06:40:57 +0700

    test entries

--- db/sqlc/entries_test.go
+++ db/sqlc/entries_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateEntry(t *testing.T) {
+func createRandomEntry(t *testing.T) Entry {
 	account := createRandomAccount(t)
 	arg := CreateEntryParams{
 		AccountID: account.ID,
@@ -19,4 +19,46 @@ func TestCreateEntry(t *testing.T) {
 	require.NotEmpty(t, entry)
 	require.Equal(t, account.ID, entry.AccountID)
 	require.Equal(t, arg.Amount, entry.Amount)
+	return entry
+}
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQuery.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+}
+
+func TestGetListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		arg := CreateEntryParams{
+			AccountID: account.ID,
+			Amount:    util.RandomBalance(),
+		}
+		entry, err := testQuery.CreateEntry(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, entry)
+		require.Equal(t, account.ID, entry.AccountID)
+		require.Equal(t, arg.Amount, entry.Amount)
+	}
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQuery.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
 }
