198ca697639652569ff19ceb8a82b9ea19108a31
Alvin KN <alvinkn.dev@gmail.com>
2023-01-19 22:00:35 +0700

    add select for no key update and update acc balanc

--- db/query/account.sql
+++ db/query/account.sql
@@ -12,9 +12,20 @@ RETURNING *;
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAccount :many
 SELECT * FROM accounts
 ORDER BY id LIMIT $1 OFFSET $2;
+
+-- name: UpdateBalance :one
+UPDATE accounts
+SET BALANCE = BALANCE + sqlc.arg(amount)
+WHERE ID = sqlc.arg(ID)
+RETURNING *;
 
 -- name: UpdateAccount :one
 UPDATE accounts
