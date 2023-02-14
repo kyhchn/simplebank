77902ecbd07b0f4872437fee33c6c7d2eb1cb2a7
Alvin KN <alvinkn.dev@gmail.com>
2023-02-14 13:31:27 +0700

    add get acc, listAcc endpoint

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,58 @@ import (
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type getListAccountRequest struct {
+	PageID   int16 `form:"page_id" binding:"required,min=1"`
+	PageSize int16 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) getListAccount(ctx *gin.Context) {
+	var req getListAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountParams{
+		Limit:  int32(req.PageSize),
+		Offset: int32(req.PageID-1) * int32(req.PageSize),
+	}
+
+	accounts, err := server.store.ListAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
